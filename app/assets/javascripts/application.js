33d38c01436b4d9f99e2e5e0ceab4fca41c38ba8
Bryan Phillips <bdphilly@gmail.com>
2014-08-13 13:46:28 -0700

    build typeahead

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
+//= require typeahead
 //= require turbolinks
 //= require underscore
 //= require backbone
--- app/assets/javascripts/views/form.js
+++ app/assets/javascripts/views/form.js
@@ -3,11 +3,12 @@ Plastiq.Views.PayeeForm = Backbone.CompositeView.extend ({
 
   events: {
     'submit': 'createNewPayee',
-    'click input[type=radio]': 'handlePlaceholder'
+    'click input[type=radio]': 'handlePlaceholder',
+    // 'typeahead:selected': 'handleTypeAheadClick'
   },
 
   initialize: function () {
-    this.listenTo(this.collection, 'sync', this.addGoogleAutocomplete);
+    this.listenTo(this.collection, 'sync', this.findFromGoogle);
     this.listenTo(this.collection, 'sync', this.handlePlaceholder);
   },
 
@@ -39,25 +40,72 @@ Plastiq.Views.PayeeForm = Backbone.CompositeView.extend ({
     }
   },
 
-  addGoogleAutocomplete: function () {
-    var input = document.getElementById('business-name');
-    var options = {
-      types: ['establishment']
-    };
+  // addGoogleAutocomplete: function () {
+  //   var input = document.getElementById('business-name');
+  //   var options = {
+  //     types: ['establishment']
+  //   };
 
-    autocomplete = new google.maps.places.Autocomplete(input, options);
+  //   autocomplete = new google.maps.places.Autocomplete(input, options);
+  // },
+
+  findFromGoogle: function() {
+    var service = new google.maps.places.AutocompleteService();
+
+    $('#business-name').typeahead({
+      highlight: true,
+      },
+
+      {
+      source: function(query, callback) {
+        service.getPlacePredictions({ 
+          input: query,
+          types: ['establishment']
+        }, 
+
+        function(predictions, status) {
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+            var predictionOutput = $.map(predictions, function (prediction) {
+               var x = { value: prediction.description }
+               // var x = prediction.description
+               return x
+                
+            })
+            // console.log(predictionOutput)
+            callback(predictionOutput)
+          }
+        });
+      },
+
+      templates: {
+        suggestion: function (item) {
+          var htmlOutput = '<div class="line-one">' + item.value.split(',')[0] + '</div>'
+          htmlOutput += '<div class="line-two">' + item.value.split(',').slice(1) + '</div>'
+          return htmlOutput;
+        },
+        footer: "<div class='footer'>No match? That’s ok. Enter any name and click the &nbsp" +
+        "<img src=http://s4.postimg.org/e6v1fkje1/arrow.png'> button.</div>",           
+      },
+    });
+    
+  },
+
+  handleTypeAheadClick: function (event, suggestion, dataset) {
+    debugger
   },
 
   handlePlaceholder: function () {
     if ($('#business').is(':checked')) {
       $('#person-name').val('');
       $('#person-name').hide();
+      $('.twitter-typeahead').show();
       var normalPlaceholder = "What business do you want to pay by card?"
       var focusedPlaceholder = "Enter the name of the business..."
       this.focusedPlaceholder('#business-name', normalPlaceholder, focusedPlaceholder);
     } else {
       $('#business-name').val('');
       $('#business-name').hide();
+      $('.twitter-typeahead').hide();
       var normalPlaceholder = "What person do you want to pay by card?"
       var focusedPlaceholder = "Enter the name of the person..."
       this.focusedPlaceholder('#person-name', normalPlaceholder, focusedPlaceholder);
